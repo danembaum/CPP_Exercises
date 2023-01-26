eda7f063a2eba3b4e5819df6c3be22b18d9abb29
danembaum <dcl.2604@gmail.com>
2023-01-26 19:08:10 -0300

    Adding C++ code of exercise 11 resolution

---
+++ ex_11.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int stringLength(char *s){
+    int count = 0;
+    for (char c = *s; c; c=*++s)
+        count++;
+    return count;
+}
+
+void reverse(char *s, int init, int end){
+    while(init<end)
+    {
+       s[init]^=s[end];
+       s[end]^=s[init];
+       s[init]^=s[end];
+       init++;
+       end--;
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+    /*Make a program in C/C++ or Java language that reverses a string informed by the user 
+    without using any temporary variable, buffer or any pre-existing function or method for this*/
+
+    char *s = argv[1];
+    int length = stringLength(s);
+
+    int start=0;
+    int end= length-1;
+    reverse(s, start, end);
+    cout<<s<<endl;
+    
+    return 0;
+    
+}
